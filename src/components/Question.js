b89527cc85341ccfbeaffd6c3d637b7d9ff9f7b4
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-07-26 19:51:24 -0700

    done with the assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
@@ -9,6 +9,21 @@ function Question({ question, onAnswered }) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (timeRemaining === 0) {
+        setTimeRemaining(10)
+        onAnswered(false)
+      }
+      else {
+        setTimeRemaining(timeRemaining - 1)
+      }
+    }, 1000)
+    return (() => {
+      clearTimeout(timer)
+    })
+  }, [timeRemaining])
 
   const { id, prompt, answers, correctIndex } = question;
 
